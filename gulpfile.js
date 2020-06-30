13ade81cb8be9232488091524f7525b32f0391ac
SergSmith64 <sergsmith2008@yandex.ru>
2020-06-30 19:36:29 +0400

    feat: css replaced by scss

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const browserSync = require('browser-sync').create();
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
 const sass = require('gulp-sass');
+// const scss = require('gulp-scss');
 const autoprefixer = require('gulp-autoprefixer');
 const cleancss = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
@@ -30,8 +31,9 @@ function scripts() {
 }
 
 function styles() {
-  return src('app/index.css')
-    // .pipe(sass())
+  return src('app/index.scss')
+    .pipe(sass())
+    // .pipe(scss())
     .pipe(concat('app.min.css'))
     .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
     // .pipe(cleancss(( { level: { 1: { specialComments: 0 } } /*, format: 'beautify' */ } )))
@@ -67,8 +69,8 @@ function buildcopy() {
 
 function startwatch() {
   watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
-  // watch('app/**/*.sass', styles);
-  watch(['app/**/*.css', '!app/**/*.min.css'], styles);
+  watch('app/**/*.scss', styles);
+  // watch(['app/**/*.css', '!app/**/*.min.css'], styles);
   watch('app/**/*.html').on('change', browserSync.reload);
   watch('app/images/src/**/*', images)
 }
